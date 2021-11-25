e0429ef19de08bb2854f6228b12e000ecc30b26c
Kelly Trinh <kvantrinh2000@gmail.com>
2021-11-25 10:52:42 -0800

    Loaded local dummy image

--- final-project/src/components/Body.js
+++ final-project/src/components/Body.js
@@ -10,7 +10,7 @@ Body component contains favorite artist and favorite tracks.
 function Body() {
 
     // dummy image url
-    const dummy_url = 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'
+    const dummy_url = './peach_cat_glasses.jpeg'
     // dummy list of favorite artists to build the favorite artist track 
     const fav_artists = [
         {
--- final-project/src/components/Individual_artist.js
+++ final-project/src/components/Individual_artist.js
@@ -1,14 +1,16 @@
 import React from 'react'
-
+import peach_cat_glasses from './peach_cat_glasses.jpeg' // dummy image 
 /*
 Each individual artist to be displayed on the favorite artists list on the homepage.
 Displaying artist's image and name. 
+
+Note: currently using dummy image
 */
 
 function Individual_artist({ artist }) {
     return (
         <>
-            <img src={artist.img_url} alt="artist image" />
+            <img src={peach_cat_glasses} alt="artist image" /> 
             <p>{artist.name}</p>
         </>
     )
