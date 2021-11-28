701308b726be914338ee6f7a8f46ef945f2c90ab
Kelly Trinh <kvantrinh2000@gmail.com>
2021-11-28 00:30:53 -0800

    Commiting all modified files

--- final-project/client/src/reducer.js
+++ final-project/client/src/reducer.js
@@ -2,9 +2,8 @@
 export const initialState = {
     user: null,
     token: null,
-    playlist: null, 
-    playings: null,
-    items: null,
+    top_artist: null,
+    top_track: null,
 }
 
 const reducer = (state, action) => {
@@ -20,6 +19,16 @@ const reducer = (state, action) => {
             return {
                 ...state, 
                 token: action.token
+            }
+        case 'SET_TOP_ARTIST': 
+            return {
+                ...state, 
+                top_artist: action.top_artist
+            }
+        case 'SET_TOP_TRACK': 
+            return {
+                ...state, 
+                top_track: action.top_track
             }
         default: 
             return state; // just return the state without modifying it
