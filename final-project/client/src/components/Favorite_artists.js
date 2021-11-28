009e7d2e35689714f115b53ee06ce3e1d6285994
Kelly Trinh <kvantrinh2000@gmail.com>
2021-11-28 00:05:40 -0800

    Linked favorited artists

---
+++ final-project/client/src/components/Favorite_artists.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Individual_artist from './Individual_artist'
+import './Body.css'
+import { useDataLayerValue } from '../../src/DataLayer'
+
+/*
+A running list of favorite artist for the home page. 
+Displays top 15 top artists. 
+*/
+
+function Favorite_artists({ artists }) {
+
+    const [{ top_artist }, dispatch] = useDataLayerValue();
+
+    return (
+        <>
+            <h1 className="header">Your favorite artists</h1>
+            <div className="fav">  
+                {top_artist?.items?.map((artist, index) => (
+                    <Individual_artist key={index} artist={artist} />
+                ))}
+            </div>
+        </>
+
+    )
+
+}
+
+export default Favorite_artists
---
+++ final-project/client/src/components/Individual_artist.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import peach_cat_glasses from './peach_cat_glasses.jpeg' // dummy image 
+import "./Body.css"
+/*
+Each individual artist to be displayed on the favorite artists list on the homepage.
+Displaying artist's image and name. 
+
+Note: currently using dummy image
+*/
+
+function Individual_artist({ artist }) {
+    return (
+        <div className="individual">
+            <img className="artist_img" src={artist.images[0].url} alt="artist image" /> 
+            <p className="name">{artist.name}</p>
+        </div>
+    )
+
+}
+
+export default Individual_artist
